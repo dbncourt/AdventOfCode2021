a92e93e4033d8d5d87167c8d086e96228adde645
Daniel Betancourt <dbncourt@gmail.com>
2022-09-03 17:14:21 +0200

    Add day 1 solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+Learning Scala while solving advent of code
+
 https://adventofcode.com/2021
--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,39 +1,44 @@
 import scala.io.Source
 
-def countIncreases(numbers: Array[Int]): Int = {
-  var change = 0
-  var previous = Int.MinValue
-  for (number <- numbers) {
-    if number > previous then
-      change += 1
-    previous = number
+class Day1 {
+
+  def countIncreases(numbers: Array[Int]): Int = {
+    var change = 0
+    var previous = Int.MinValue
+    for (number <- numbers) {
+      if number > previous then
+        change += 1
+      previous = number
+    }
+    change - 1
   }
-  change - 1
+
+   def countGroupedIncreases(numbers: Array[Int]): Int = {
+    var change = 0
+    var next = numbers(1)
+    var current = numbers(0) + next
+    var previous = Int.MaxValue
+
+    for (i <- 2 until numbers.length) {
+      current = current + numbers(i)
+      if (current > previous)
+        change += 1
+
+      previous = current
+      current = next + numbers(i)
+      next = numbers(i)
+    }
+    change
+  }
 }
 
-def countGroupedIncreases(numbers: Array[Int]): Int = {
-  var change = 0
-  var next = numbers(1)
-  var current = numbers(0) + next
-  var previous = Int.MaxValue
-
-  for (i <- 2 until numbers.length) {
-    current = current + numbers(i)
-    if (current > previous)
-      change += 1
-
-    previous = current
-    current = next + numbers(i)
-    next = numbers(i)
-  }
-  change
-}
 
 @main def main(): Unit = {
+  val day1 = Day1()
   val file = Source.fromResource("day1/input1.txt")
   val numbers = file.getLines().map(line => line.toInt).toArray
-  val part1 = countIncreases(numbers)
+  val part1 = day1.countIncreases(numbers)
   println(s"Part 1 increases: $part1")
-  val part2 = countGroupedIncreases(numbers)
+  val part2 = day1.countGroupedIncreases(numbers)
   println(s"Part 1 increases: $part2")
 }
